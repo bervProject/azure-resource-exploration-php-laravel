2f01977ad1a7dd56d894e67663a5513f56449de5
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-09-08 19:40:10 +0700

    Migrate from Laravel Mix to Vite

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,14 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Azure Exploration</title>
-    <!-- Styles -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 <div id="app">
     @yield('content')
 </div>
-<script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
 @yield('script')
 </body>
 </html>
